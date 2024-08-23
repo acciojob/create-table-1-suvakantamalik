4c86a25220b464e92d11894bb054d08f348e7d06
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-08-23 13:05:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+    //Write your code here	
+	const tbl = document.getElementById("sampleTable");
+	let rowCount = tbl.rows.length;
+	let newRowCt = rowCount+1;
+    // console.log("Total number of rows: " + rowCount);
+	
+	let newRow = document.createElement("tr");
+	
+	let newCell1 = document.createElement("td");
+	newCell1.textContent = `Row${newRowCt} cell1`;
+	
+	let newCell2 = document.createElement("td");
+	newCell2.textContent = `Row${newRowCt} cell2`;
+
+	newRow.appendChild(newCell1);
+	newRow.appendChild(newCell2);
+
+	tbl.appendChild(newRow);
 }
